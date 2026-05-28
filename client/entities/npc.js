3e5200bdddc9f873e9ebb1dcb87b3226dab48ab2
g0ry23 <g0ry23@icloud.com>
2026-05-28 02:44:14 -0500

    feat: split client/entities/npc.js

---
+++ client/entities/npc.js
@@ -0,0 +1,9 @@
+(function(){
+  const D = window.Duskfall = window.Duskfall || {};
+  D.createNPC=function(id,x,y){
+    const cfg=D.NPCS[id];
+    return {uid:D.uid('npc'),kind:'npc',id,cfg,x,y,r:22,color:id==='banker'?'#f7c65b':id==='trader'?'#63e6a4':'#6aa7ff'};
+  };
+})();
+</script>
+<script>
