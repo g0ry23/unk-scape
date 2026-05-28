75497f26d7ef302c4eb1ab127be9e85a80a22070
g0ry23 <g0ry23@icloud.com>
2026-05-28 11:12:41 -0500

    fix: toasts.js target toast-container not toasts + null guard

--- client/ui/toasts.js
+++ client/ui/toasts.js
@@ -1,11 +1,10 @@
 (function(){
   const D = window.Duskfall = window.Duskfall || {};
   D.UI.prototype.toast=function(title,msg,type=''){
-    const root=document.getElementById('toasts'), id=++this.toastId;
+    const root=document.getElementById('toast-container'), id=++this.toastId;
+    if(!root) return;
     const div=document.createElement('div');div.className='toast '+type;div.dataset.id=id;div.innerHTML=`<b>${title}</b>${msg}`;root.appendChild(div);
     setTimeout(()=>{div.style.opacity='0';div.style.transform='translateY(-8px)';},3300);
     setTimeout(()=>div.remove(),3800);
   };
 })();
-</script>
-<script>
