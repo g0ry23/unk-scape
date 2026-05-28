2564d8a3adc91945e69dd95a2058c34618f0e9ab
g0ry23 <g0ry23@icloud.com>
2026-05-28 12:24:26 -0500

    fix: strip injected HTML from portal.js

--- client/entities/portal.js
+++ client/entities/portal.js
@@ -2,5 +2,3 @@
   const D = window.Duskfall = window.Duskfall || {};
   D.createPortal=function(id,x,y){return{uid:D.uid('portal'),kind:'portal',id,x,y,r:32,name:id==='dungeon'?'Old Catacombs':'Portal'}};
 })();
-</script>
-<script>
