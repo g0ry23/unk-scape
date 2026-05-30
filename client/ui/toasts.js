aee6e2d76ae72eaccc1fc36ded037e5795bc7eeb
g0ry23 <g0ry23@icloud.com>
2026-05-30 17:39:22 -0500

    scorched-earth: toasts.js cord-cut

--- client/ui/toasts.js
+++ client/ui/toasts.js
@@ -1,6 +1,5 @@
 (function(){
-  window.Duskfall = window.Duskfall || {};
-const US = window.UnkScape = window.Duskfall;
+  const US = window.UnkScape = window.UnkScape || {};
   US.UI.prototype.toast=function(title,msg,type=''){
     const root=document.getElementById('toast-container'), id=++this.toastId;
     if(!root) return;
