dcefc871bdfe5ada5bcfdc98d531d17a5a93c320
g0ry23 <g0ry23@icloud.com>
2026-05-30 17:00:57 -0500

    scorched-earth: npc.js cord-cut

--- client/entities/npc.js
+++ client/entities/npc.js
@@ -1,6 +1,5 @@
 (function(){
-  window.Duskfall = window.Duskfall || {};
-const US = window.UnkScape = window.Duskfall;
+  const US = window.UnkScape = window.UnkScape || {};
   US.createNPC=function(id,x,y){
     const cfg=US.NPCS[id];
     return {uid:US.uid('npc'),kind:'npc',id,cfg,x,y,r:22,color:id==='banker'?'#f7c65b':id==='trader'?'#63e6a4':'#6aa7ff'};
